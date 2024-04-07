2de637d9fab810f2725639df67c7e37ff541dc87
Manish Acharya <154651472+manish-ach@users.noreply.github.com>
2024-04-07 10:31:54 +0545

    SimpleParkSystem.c
    counts the number of inputs then calculate amount.

--- Untitled1.c
+++ Untitled1.c
@@ -9,9 +9,10 @@ struct number{
 	int nubu,num,nub,*bu_amount,*m_amount,*b_amount;
 };
 
-int count1=0,count2=0,count3=0;
+int count1=0,count2=0,count3=0,number=0;
+char name[10];
 
-int options();
+int options(int*);
 void tb(struct number n);
 int bus(int,int *);
 int micro(int,int *);
@@ -24,7 +25,7 @@ int main()
 {
     
 	struct number n;
-    int a,sw;
+    int a,sw,*chk=0;
     	
 	n.nubu=0;
 	n.num=0;
@@ -35,7 +36,7 @@ int main()
     top:
     printf("\n\t\t\t******welcome to the Parking System******\n\n\n");
     tb(n);
-	sw = options();
+	sw = options(&chk);//chk is a checking variable in case of default
 	
 	switch(sw)
 	{
@@ -53,11 +54,11 @@ int main()
 			break;	
 		case 4:
 			system("cls");
-            //showdetail();
+            showdetail();
 			break;
 		case 5:
 		   system("cls");
-           //Delete();
+           Delete();
 		   break;	
 		case 6:
 		    system("cls");
@@ -65,33 +66,22 @@ int main()
 		    break;
 		 default:
             system("cls");
-		    printf("pls enter the required choice");
+		    chk=1;
 		    break;		
 	}
-
-	fi(n);
+	if (sw==1 || sw==2 || sw==3)//file specified to be created only when inputing value so it wont recreate a file after deleting it.
+	{
+		fi(n);
+	}
     system("cls");
     goto top;
 	
     return 0;
 }
 
-void fi(struct number n)
-{
-	FILE *file;
-    char filename[] = "output.txt";
-    char text[] = "vehicle    number    cost";
-    file = fopen(filename, "r+");
-    if(file==NULL)
-    {
-   	    file=fopen("output.txt","w+");
-   	    fprintf(file, "%s\n", text);
-    }
-    fprintf(file, "%d\n",n.bu_amount);   
-   	fclose(file);
-}
 
-int options()
+
+int options(int *chk1)
 {
 	int opt;
     printf("\n [1] Enter Bus records ");
@@ -100,10 +90,43 @@ int options()
 	printf("\n [4] View Details");
 	printf("\n [5] Delete Data");
 	printf("\n [6] Exit");
+	if(*chk1==1) //pointer variable used in case if the option inputted by user isnt valid.
+	{
+		printf("\n\nplease enter valid option");
+		*chk1=0;
+	}
 	printf("\n\n --> ");
     scanf("%d",&opt);
     return opt;
 }
+
+
+
+void fi(struct number n)
+{
+	FILE *file;
+    char filename[] = "log.txt";
+    int amount;
+    if(strcmp(name,"Bus")==0)
+    {
+    	amount=1000;
+	}
+	if(strcmp(name,"Micro")==0)
+    {
+    	amount=1000;
+	}
+	if(strcmp(name,"Bike")==0)
+    {
+    	amount=1500;
+	}
+    number++;
+    file = fopen(filename, "a+");//read and write, creates new if file doesnt exist
+    //number	vehicle     cost";
+    fprintf(file, "%d\t%s\t%d\n",number,name,amount);   
+   	fclose(file);
+}
+
+
 
 void tb(struct number n)
 {
@@ -136,25 +159,80 @@ void tb(struct number n)
 	
 }
 
+
+
 int bus(int nobu,int *amount1)
 {    
+	strcpy(name,"Bus");
 	count1++;
 	nobu++;
 	*amount1=*amount1+1000;
 	return nobu;
 }
 
+
+
 int micro(int nom,int *amount2)
 {    
+	strcpy(name,"Micro");
 	count2++;
 	nom++;
-	*amount2=*amount2+1050;
+	*amount2=*amount2+1000;
 	return nom;
 }
 
+
+
 int bike(int nob,int *amount3)
 {    
+	strcpy(name,"Bike");
 	nob++;
 	*amount3=*amount3+1500;
 	return nob;
+}
+
+void Delete()
+{
+	char a;
+	printf("Do you want to delete the records?(y/n) - ");
+	a=getch();
+	
+	if(a=='y')
+	{
+		FILE *fp;
+		fp=fopen("log.txt","w");	
+        fclose(fp);
+	}
+
+}
+
+
+void showdetail()
+{
+	FILE *file;
+	int nmb, amnt, counter=0;
+	char veh[10], a;
+	file = fopen("log.txt", "r");
+	printf("\nnumber\tvehicle\tamount\n");
+	printf("----------------------\n");
+	if(file==NULL)
+	{
+		printf("\n record file doesnt exist\n");
+	}
+	else
+	{
+		while((fscanf(file,"%d%s%d",&nmb,&veh,&amnt))!=EOF)
+		{
+			printf("%d\t%s\t%d\n",nmb,veh,amnt);
+			counter++;
+		}
+	}
+	if(counter==0)
+	{
+		printf("\n");
+		printf("Sorry, no records found.\n");
+	}
+	fclose(file);
+	printf("\ncontinue?(press any button)");
+	a=getch();
 }
